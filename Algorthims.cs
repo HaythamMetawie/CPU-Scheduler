dc4fb4ef401c615869363e3ab9ab2a7e58a0f442
HaythamMetawie <haytham.h3@gmail.com>
2016-03-22 06:05:48 +0200

    Update Algorthims.cs

--- Algorthims.cs
+++ Algorthims.cs
@@ -8,40 +8,50 @@ namespace Algorithms
 {
     class Algorithms
     {
-        private float waitingtime, turnaround;
+        private double waitingtime, turnaround;
         private int NumOfProcesses;
-        public float getWaitingtime()
+        public double getWaitingtime()
         {
             return waitingtime;
         }
-        public float Waitingtime(float turnaround, int burst)
-        {
-            return waitingtime = (turnaround - burst) / NumOfProcesses;
-        }
         public void FCFS()
         {
-            LinkedList<process> Processes = new LinkedList<process>();
-            process [] p = new process[NumOfProcesses];
-            int[] Burst = new int[NumOfProcesses];
-            float[] Arrival = new float[NumOfProcesses];
+            LinkedList<process> processes = new LinkedList<process>();
+            LinkedList<process> sorted_processes = new LinkedList<process>();
+            double[] Burst = new double[NumOfProcesses];
+            process[] p = new process[NumOfProcesses];
+            double[] Arrival = new double[NumOfProcesses];
             Console.WriteLine("Enter Number of Processes:");
             NumOfProcesses = Console.Read();
             for (int i = 0; i < NumOfProcesses; i++)
             {
                 Console.WriteLine("Enter Arrival Time of Process", i + 1, ":");
                 Arrival[i] = Console.Read();
-                Console.WriteLine("Enter Burst Time of Process", i+1,":");
-                Burst [i]  = Console.Read();
+                Console.WriteLine("Enter Burst Time of Process", i + 1, ":");
+                Burst[i] = Console.Read();
                 p[i] = new process(Burst[i], Arrival[i]);
             }
-            for (int i = 0; i < NumOfProcesses; i++) //sorting processes according to arrival time (need to be checked)
-            {
-                if (Arrival[i] < Arrival[i + 1])
-                { Processes.AddFirst(p[i]);}
-            }
-
-
         }
+        
+        public double find_minimum(double[] a, int n)
+        {
+            double minimum = a[0]; int index = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (minimum > a[i]) { index = i;  minimum = a[i]; }
+            }
+            return minimum = a[index];
+        } 
+       /* public void sort(int n) // sorting linked list according to arrival time and put it in new one.
+        {
+
+            public void sort (int n-1) { 
+                        find_minimum(Arrival[],NumOfProcesses); 
+                        processes.
+        }
+
+        }*/
+        
 
     };
 }
